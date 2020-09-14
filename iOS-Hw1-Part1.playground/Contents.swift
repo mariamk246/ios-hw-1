520c50f69a524fe2c10721cd53ba6f51506745ca
mariam <mariooom456@gmail.com>
2020-09-14 12:59:08 +0300

    done hw 1

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -7,10 +7,10 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+var name2 = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,18 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Mariam"
+var lastName1 = "Alkandari"
+var email1 = "marioom456@gmail.com"
+var phoneNumber1 = "90901060"
+var age1 = 18
+var country1 = "Kuwait"
+var area1 = "Surra"
+var password1 = "qwert"
+var isKuwaiti1 = true
 
 //Person 2
-
+var firstName2 = "Dalal"
+var lastName2 = "Alnajem"
+var email2 =  "dalalalnajem@gmail.com"
+var phoneNumber2 = "99687768"
+var age2 = 25
+var country2 = "Kuwait"
+var area2 = "Sabah Alsalem"
+var password2 = "unicorn"
+var isKuwaiti2 = true
 
 
 
@@ -60,7 +68,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
+var firstName3 = " Judy"
+var lastName3 = " Smith"
+var email3 = " Judysmith1@gmail.com"
+var phoneNumber3 = " 992330021"
+var age3 = 21
+var country3 = " South Africa "
+var area3 = " bayan "
+var password3 = " zxcvb"
+var isKuwaiti3 = false
 
 
 
@@ -82,3 +98,32 @@ var isKuwaiti = false
 
  
  */
+var ageDiffrence = age2 - age1
+var ageDiffrence2 = age2 - age3
+var ageDiffrence3 = age3 - age1
+print (ageDiffrence)
+print (ageDiffrence2)
+print(ageDiffrence3)
+
+var areaDiffrence = area1 == area2
+var areaDiffrence2 = area2 == area3
+var areaDiffrence3 = area1 == area3
+
+age1 > 18
+age2 > 18
+age3 > 18
+ 
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
+
+
+
